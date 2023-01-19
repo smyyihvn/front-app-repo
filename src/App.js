faac92336fd91997e8664dd2bda532c24b873314
EC2 Default User <ec2-user@ip-172-31-92-115.ec2.internal>
2023-01-19 11:17:56 +0000

    Add new blog version

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog
+            EKS DEMO Blog version Gogo
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
